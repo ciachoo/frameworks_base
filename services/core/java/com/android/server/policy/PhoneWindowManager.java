omnirom/android_frameworks_base_old

base: SystemUI: add OmniSwitch hide on home
and fix a minor bug with status bar logo visibility

Change-Id: If21bccc3c6f8116a21508a2634c3ece25c100311

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3535,6 +3535,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     return -1;
                 }
 
+                if (mOmniSwitchRecents) {
+                    OmniSwitchConstants.hideOmniSwitchRecents(mContext, UserHandle.CURRENT);
+                }
+
                 handleShortPressOnHome();
                 return -1;
             }

ciachoo/frameworks_base

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -4140,6 +4140,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             // Remember that home is pressed and handle special actions.
             if (repeatCount == 0) {
                 mHomePressed = true;
+                if (mOmniSwitchRecents) {
+                    AicpUtils.hideOmniSwitchRecents(mContext, UserHandle.CURRENT);
+                }
                 if (mHomeDoubleTapPending) {
                     mHomeDoubleTapPending = false;
                     mHandler.removeCallbacks(mHomeDoubleTapTimeoutRunnable);