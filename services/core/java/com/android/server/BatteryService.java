7819e9eb272f258f73d7eddb6b4a05b980cdb556
Michael W <baddaemon87@gmail.com>
2017-10-09 22:04:00 +0200

    Core: Battery warning levels are inclusive, not exclusive
    * Instead of checking for the current level to be smaller than the warning
      level, already react when the warning level is reached
    * Simply change of '<' to '<='
    
    Bugbash-1100
    
    Change-Id: Ib35b154b6117f7e26b4a3a5aee9254dda3adda12

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -1019,7 +1019,7 @@ private boolean isDashCharger() {
             if (!mLightEnabled || (mIsDndActive && !mAllowBatteryLightOnDnd)) {
                 // No lights if explicitly disabled
                 mBatteryLight.turnOff();
-            } else if (level < mLowBatteryWarningLevel) {
+            } else if (level <= mLowBatteryWarningLevel) {
                 mBatteryLight.setModes(mNotificationLedBrightnessLevel,
                         mMultipleLedsEnabled);
                 if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
